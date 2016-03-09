9c0adad527b7438cf16cc40a81274fa725ca49b4
Michael Epstein <@mike4aday>
2016-03-09 13:15:49 -0600

    Minor fix to code indentation

--- Example/SwiftlySalesforce/AppDelegate.swift
+++ Example/SwiftlySalesforce/AppDelegate.swift
@@ -16,22 +16,20 @@ import SafariServices
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, LoginViewPresentable {
-
 	
 	/// Salesforce Connected App settings
 	let consumerKey = "3MVG91ftikjGaMd_SSivaqQgkik_rz_GVRYmFpDR6yDaUrEfpC0vKqisPMY1klyH78G9Ockl2p7IJuqRk07nQ"
 	let redirectURL = NSURL(string: "taskforce://authorized")!
 	
-    var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-				
+	var window: UIWindow?
+	
+	
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Configure the Salesforce authentication manager with Connected App settings
 		OAuth2Manager.sharedInstance.configureWithConsumerKey(consumerKey, redirectURL: redirectURL)
 		OAuth2Manager.sharedInstance.authenticationDelegate = self
-        return true
-    }
+		return true
+	}
 	
 	func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
 		handleRedirctURL(url)
