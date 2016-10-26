87adcfbac2d826cb734526b276e2c69462870bc5
Michael Epstein <@mike4aday>
2016-10-25 21:20:18 -0500

    Updated for Swift 3

--- Example/SwiftlySalesforce/TaskStore.swift
+++ Example/SwiftlySalesforce/TaskStore.swift
@@ -2,58 +2,49 @@
 //  TaskStore.swift
 //  Example for SwiftlySalesforce
 //
-//  For license & details see: https://www.github.com/mike4aday/SwiftlySalesforce
-//  Copyright (c) 2016. All rights reserved.
+//  Created by Michael Epstein on 10/21/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
 //
 
 import Foundation
-import SwiftlySalesforce
 import PromiseKit
-
+import SwiftlySalesforce
 
 public final class TaskStore {
 	
 	/// Singleton
-	public static let sharedInstance = TaskStore()
+	public static let shared = TaskStore()
 	
-	public private(set) var cache: [Task]?
+	public fileprivate(set) var cache: [Task]?
 	
-	public func getTasks(refresh refresh: Bool) -> Promise<[Task]> {
+	public func getTasks(refresh: Bool = false) -> Promise<[Task]> {
 		
 		return Promise<[Task]> {
-			
-			(fulfill, reject) -> () in
-			
-			if let tasks = self.cache where !refresh {
+			fulfill, reject in
+			if let tasks = self.cache, !refresh {
 				fulfill(tasks)
 			}
 			else {
 				firstly {
-					SalesforceAPI.Identity.request()
+					// Get ID of current user
+					//TODO: if user already authorized, then we could just get this from salesforce.authData
+					salesforce.identity()
 				}.then {
-					// Get user ID
-					(identityInfo) -> String in
-					guard let userID = identityInfo["user_id"] as? String else {
-						throw NSError(domain: "TaskForce", code: -100, userInfo: nil)
+					// Get tasks owned by user
+					userInfo in
+					guard let userID = userInfo.userID else {
+						throw TaskForceError.generic(code: 100, message: "Can't determine user ID")
 					}
-					return userID
-				}.then {
-					// Query tasks owned by user
-					(userID) -> Promise<AnyObject> in
 					let soql = "SELECT Id,Subject,Status,What.Name FROM Task WHERE OwnerId = '\(userID)' ORDER BY CreatedDate DESC"
-					return SalesforceAPI.Query(soql: soql).request()
+					return salesforce.query(soql: soql)
 				}.then {
-					// Parse JSON response into Task instances
-					(result) -> () in
-					guard let records = result["records"] as? [[String: AnyObject]] else {
-						throw NSError(domain: "TaskForce", code: -101, userInfo: nil)
-					}
-					let tasks = records.map { Task(dictionary: $0) }
+					// Parse JSON into Task instances and cache in memory
+					(result: QueryResult) -> () in
+					let tasks = result.records.map { Task(dictionary: $0) }
 					self.cache = tasks
 					fulfill(tasks)
-				}.error {
-					// Errors, e.g. loss of Internet connectivity, will be caught here
-					(error) -> Void in
+				}.catch {
+					error in
 					reject(error)
 				}
 			}
@@ -61,58 +52,7 @@ public final class TaskStore {
 	}
 	
 	/// Called on logout to clear locally-cached data
-	internal func clear() {
+	public func clear() {
 		cache = nil
-	}
-}
-
-
-public final class Task {
-	
-	public var id: String?
-	public var subject: String?
-	public var dueDate: NSDate?
-	public var createdDate: NSDate?
-	public var whatName: String?
-	public var whatType: String?
-	public var status: String?
-	public var priority: String?
-	public var isHighPriority: Bool = false
-	
-	public init() { }
-	
-	/// Creates instance from JSON returned by Salesforce REST API
-	/// - Parameter dictionary: JSON object returned by Salesforce
-	public init(dictionary: [String: AnyObject]) {
-		
-		for (key, value) in dictionary {
-			switch key.lowercaseString {
-			case "id":
-				self.id = value as? String
-			case "subject":
-				self.subject = value as? String
-			case "activitydate":
-				if let s = value as? String {
-					self.dueDate = NSDateFormatter.SalesforceDate.dateFromString(s)
-				}
-			case "createddate":
-				if let s = value as? String {
-					self.createdDate = NSDateFormatter.SalesforceDateTime.dateFromString(s)
-				}
-			case "what":
-				if let dict = value as? [String: AnyObject] {
-					self.whatName = dict["Name"] as? String
-					self.whatType = dict["Type"] as? String
-				}
-			case "status":
-				self.status = value as? String
-			case "priority":
-				self.priority = value as? String
-			case "ishighpriority":
-				self.isHighPriority = (value as? Int) == 1
-			default:
-				continue
-			}
-		}
 	}
 }
