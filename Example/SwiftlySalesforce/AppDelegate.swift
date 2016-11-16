f277f9eb5d98ef456f13aaea516318e3de39bbc4
Michael Epstein <@mike4aday>
2016-11-16 09:59:25 -0600

    Call handleRedirectURL(url: URL) instead of deprecated handleRedirectURL(redirectURL: URL)

--- Example/SwiftlySalesforce/AppDelegate.swift
+++ Example/SwiftlySalesforce/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LoginDelegate {
 	var window: UIWindow?
 	
 	/// Salesforce Connected App properties
-	let consumerKey = "3MVG91ftikjGaM__SSivaqQgkik_rz_GSRYmFpDR6yDaUrEfpC0vKqisPMY1ilyH78G9Ocpl2p7IJuqRk07nQ"
-	let redirectURL = URL(string: "taskforce://authorized")!
+	let consumerKey = "<YOUR SALESFORCE CONNECTED APP'S CONSUMER KEY>"
+	let redirectURL = URL(string: "<YOUR SALESFORCE CONNECTED APP'S REDIRECT URL>")! 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		configureSalesforce(consumerKey: consumerKey, redirectURL: redirectURL)
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LoginDelegate {
 	}
 	
 	func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-		handleRedirectURL(redirectURL: url as URL)
+		handleRedirectURL(url: url as URL)
 		return true
 	}
 }
