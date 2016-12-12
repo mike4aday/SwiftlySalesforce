a11d81ab33c3f86d5401195b9a421af09fb7d74e
Michael Epstein <@mike4aday>
2016-12-11 21:28:54 -0600

    Added Collection extension for transformation to Dictionary

--- Pod/Classes/Extensions.swift
+++ Pod/Classes/Extensions.swift
@@ -81,3 +81,17 @@ public extension UIApplicationDelegate where Self: LoginDelegate {
 		salesforce.version = version
 	}
 }
+
+/// Transform a collection into a dictionary
+/// From: https://gist.github.com/ijoshsmith/0c966b1752b9a5722e23
+public extension Collection {
+	func asDictionary<K, V>(transform:(_ element: Iterator.Element) -> [K : V]) -> [K : V] {
+		var dictionary = [K : V]()
+		self.forEach { element in
+			for (key, value) in transform(element) {
+				dictionary[key] = value
+			}
+		}
+		return dictionary
+	}
+}
