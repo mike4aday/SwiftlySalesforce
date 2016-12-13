c37a28e15e5cdaf0ef408fe4ddd5de198d333309
Michael Epstein <@mike4aday>
2016-12-13 09:50:55 -0600

    Added support for testing SObjectDescribe results and model classes

--- Example/Tests/MockJSONData.swift
+++ Example/Tests/MockJSONData.swift
@@ -9,30 +9,38 @@
 import Foundation
 
 protocol MockJSONData: class {
-	var identityJSON: Any? { get }
-	var queryResultJSON: Any? { get }
+	var identityJSON: [String: Any]? { get }
+	var queryResultJSON: [String: Any]? { get }
+	var describeAccountResultJSON: [String: Any]? { get }
+	var describeTaskResultJSON: [String: Any]? { get }
 }
 
 extension MockJSONData {
 	
-	func read(fileName: String, ofType: String = "json") -> Any? {
-		
+	func read<T>(fileName: String, ofType: String = "json") -> T? {
 		guard let path = Bundle(for: type(of: self)).path(forResource: fileName, ofType: ofType) else {
 			return nil
 		}
 		let url = URL(fileURLWithPath: path)
-		guard let data = try? Data(contentsOf: url),
-			let json = try? JSONSerialization.jsonObject(with: data) else {
+		guard let data = try? Data(contentsOf: url), let json = try? JSONSerialization.jsonObject(with: data) else {
 			return nil
 		}
-		return json
+		return json as? T
 	}
 	
-	var identityJSON: Any? {
-		return read(fileName: "IdentityResponse", ofType: "json")
+	var identityJSON: [String: Any]? {
+		return read(fileName: "IdentityResult", ofType: "json")
 	}
 	
-	var queryResultJSON: Any? {
-		return read(fileName: "QueryResponse", ofType: "json")
+	var queryResultJSON: [String: Any]? {
+		return read(fileName: "QueryResult", ofType: "json")
+	}
+	
+	var describeAccountResultJSON: [String: Any]? {
+		return read(fileName: "DescribeAccountResult", ofType: "json")
+	}
+	
+	var describeTaskResultJSON: [String: Any]? {
+		return read(fileName: "DescribeTaskResult", ofType: "json")
 	}
 }
