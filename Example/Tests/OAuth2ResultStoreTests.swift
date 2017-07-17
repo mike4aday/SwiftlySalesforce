d3441d750c39a68a418a5d06f298cf78207d8fbc
Michael Epstein <mike4aday@users.noreply.github.com>
2017-07-17 09:21:43 -0500

    Update OAuth2ResultStoreTests.swift

--- Example/Tests/OAuth2ResultStoreTests.swift
+++ Example/Tests/OAuth2ResultStoreTests.swift
@@ -3,7 +3,7 @@
 //  SwiftlySalesforce
 //
 //  Created by Michael Epstein on 7/13/17.
-//  Copyright © 2017 CocoaPods. All rights reserved.
+//  Copyright (c) 2017 CocoaPods. All rights reserved.
 //
 
 import XCTest
