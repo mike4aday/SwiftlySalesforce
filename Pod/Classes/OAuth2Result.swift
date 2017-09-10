76c3db4efcfccf961188d35c90c4ed9e2b38aa99
Michael Epstein <@mike4aday>
2017-09-10 16:02:11 -0500

    Removed 'import Locksmith'

--- Pod/Classes/OAuth2Result.swift
+++ Pod/Classes/OAuth2Result.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2017. All rights reserved.
 //
 
-import Locksmith
-
 /// Holds result of successful OAuth2 user-agent flow
 /// See https://help.salesforce.com/articleView?id=remoteaccess_oauth_user_agent_flow.htm&type=0
 internal struct OAuth2Result {
@@ -33,6 +31,12 @@ internal struct OAuth2Result {
 	// MARK: - Initializers
 	//
 	
+	/// Initializer
+	/// - Parameter accessToken: Salesforce access token (session ID).
+	/// - Parameter instanceURL: Salesforce instance to use for subsequent, authenticated requests.
+	/// - Parameter identityURL: Salesforce identity URL unique to the current user. See: https://help.salesforce.com/articleView?id=remoteaccess_using_openid.htm&type=0
+	/// - Parameter refreshToken: Optional value for refresh token; since the refresh token flow doesn't return the refresh token itself,
+	///   including the refresh token as an argument will preserve it for the next refresh request.
 	internal init(accessToken: String, instanceURL: URL, identityURL: URL, refreshToken: String?) {
 		self.accessToken = accessToken
 		self.instanceURL = instanceURL
@@ -43,7 +47,7 @@ internal struct OAuth2Result {
 	/// Initialize credentials from a Salesforce-produced redirect URL, to which
 	/// URL-encoded token information has been appended in the fragment
 	/// - Parameter urlEncodedString: URL fragment or query string as returned by Salesforce during OAuth2 user-agent or refresh token flow
-	/// - Parameter refreshToken: Optional value for refresh token; since the refresh token flow doesn't return the refresh token itself, 
+	/// - Parameter refreshToken: Optional value for refresh token; since the refresh token flow doesn't return the refresh token itself,
 	///   including the refresh token as an argument will preserve it for the next refresh request.
 	internal init(urlEncodedString: String, refreshToken: String? = nil) throws {
 		
@@ -55,7 +59,7 @@ internal struct OAuth2Result {
 		guard let accessToken = url.value(forQueryItem: "access_token"),
 			let instanceURL = URL(string: url.value(forQueryItem: "instance_url")),
 			let identityURL = URL(string: url.value(forQueryItem: "id")) else {
-			throw SerializationError.invalid(urlEncodedString, message: "Can't parse URL-encoded string: \(urlEncodedString)")
+				throw SerializationError.invalid(urlEncodedString, message: "Can't parse URL-encoded string: \(urlEncodedString)")
 		}
 		
 		let refreshToken = refreshToken ?? url.value(forQueryItem: "refresh_token")
