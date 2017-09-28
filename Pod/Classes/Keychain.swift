474ce7ec409b3ce4087f64350c4c9847900fbdda
Michael Epstein <@mike4aday>
2017-09-28 11:06:10 -0500

    --

--- Pod/Classes/Keychain.swift
+++ Pod/Classes/Keychain.swift
@@ -8,92 +8,135 @@
 
 import Foundation
 
-/// Simple wrapper around keychain for secure storage.
-/// Adapted from https://developer.apple.com/library/content/samplecode/GenericKeychain/Listings/GenericKeychain_KeychainPasswordItem_swift.html
-
-internal final class Keychain {
+/// Utility routines for working with the keychain.
+/// Borrowed from https://forums.developer.apple.com/thread/86961
+enum Keychain {
 	
-	static func write(data: Data, service: String, account: String) throws {
-		
-		do {
-			// Check for an existing item in the keychain.
-			try _ = read(service: service, account: account)
-			
-			// Update the existing item with the new password.
-			var attributesToUpdate = [String : AnyObject]()
-			attributesToUpdate[kSecValueData as String] = data as AnyObject?
-			
-			let query = buildQuery(service: service, account: account)
-			let status = SecItemUpdate(query as CFDictionary, attributesToUpdate as CFDictionary)
-			
-			// Throw an error if an unexpected status was returned.
-			guard status == noErr else {
-				throw KeychainError.writeFailure(status: status)
-			}
-		}
-		catch KeychainError.itemNotFound {
-			
-			// No password was found in the keychain. Create a dictionary to save as a new keychain item.
-			var newItem = buildQuery(service: service, account: account)
-			newItem[kSecValueData as String] = data as AnyObject?
-			
-			// Add a the new item to the keychain.
-			let status = SecItemAdd(newItem as CFDictionary, nil)
-			
-			// Throw an error if an unexpected status was returned.
-			guard status == noErr else {
-				throw KeychainError.writeFailure(status: status)
-			}
+	/// Returns the value of a generic password keychain item.
+	///
+	/// - Parameters:
+	///   - service: The service name for the item.
+	///   - account: The account for the item.
+	/// - Returns: The value of the item
+	/// - Throws: Any error returned by the Security framework.
+	
+	static func read(service: String, account: String) throws -> Data {
+		var copyResult: CFTypeRef? = nil
+		let err = SecItemCopyMatching([
+			kSecClass: kSecClassGenericPassword,
+			kSecAttrService: service,
+			kSecAttrAccount: account,
+			kSecReturnData: true
+			] as NSDictionary, &copyResult)
+		switch err {
+		case errSecSuccess:
+			return copyResult as? Data ?? Data()
+		case errSecItemNotFound:
+			throw KeychainError.itemNotFound
+		default:
+			throw KeychainError.readFailure(status: err)
 		}
 	}
 	
-	static func read(service: String, account: String) throws -> Data  {
-		
-		// Build a query to find the item that matches the service and account
-		var query = buildQuery(service: service, account: account)
-		query[kSecMatchLimit as String] = kSecMatchLimitOne
-		query[kSecReturnAttributes as String] = kCFBooleanTrue
-		query[kSecReturnData as String] = kCFBooleanTrue
-		
-		// Try to fetch the existing keychain item that matches the query.
-		var queryResult: AnyObject?
-		let status = withUnsafeMutablePointer(to: &queryResult) {
-			SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0))
+	/// Stores a value to a generic password keychain item.
+	///
+	/// This delegates the work to two helper routines depending on whether the item already
+	/// exists in the keychain or not.
+	///
+	/// - Parameters:
+	///   - service: The service name for the item.
+	///   - account: The account for the item.
+	///   - data: The desired data.
+	/// - Throws: Any error returned by the Security framework.
+	
+	static func write(data password: Data, service: String, account: String) throws {
+		var copyResult: CFTypeRef? = nil
+		let err = SecItemCopyMatching([
+			kSecClass: kSecClassGenericPassword,
+			kSecAttrService: service,
+			kSecAttrAccount: account,
+			kSecReturnData: true
+			] as NSDictionary, &copyResult)
+		switch err {
+		case errSecSuccess:
+			let oldPassword = copyResult as? Data ?? Data()
+			if oldPassword != password {
+				try self.storeByUpdating(service: service, account: account, password: password)
+			}
+		case errSecItemNotFound:
+			try self.storeByAdding(service: service, account: account, password: password)
+		default:
+			throw KeychainError.writeFailure(status: err)
 		}
-		
-		// Check the return status and throw an error if appropriate.
-		guard status != errSecItemNotFound else {
-			throw KeychainError.itemNotFound(service: service, account: account)
-		}
-		guard status == noErr else {
-			throw KeychainError.readFailure(status: status)
-		}
-		
-		// Parse the data from the query result.
-		guard let existingItem = queryResult as? [String : AnyObject], let data = existingItem[kSecValueData as String] as? Data else {
-			throw KeychainError.itemHasNoData(service: service, account: account)
-		}
-		
-		return data
 	}
 	
+	/// Deletes an item from the keychain.
+	///
+	/// - Parameters:
+	///   - service: The service name for the item.
+	///   - account: The account for the item.
 	static func delete(service: String, account: String) throws {
 		
 		// Delete the existing item from the keychain.
-		let query = buildQuery(service: service, account: account)
-		let status = SecItemDelete(query as CFDictionary)
+		let err = SecItemDelete([
+			kSecClass: kSecClassGenericPassword,
+			kSecAttrService: service,
+			kSecAttrAccount: account,
+		] as NSDictionary)
 		
-		// Throw an error if an unexpected status was returned.
-		guard status == noErr || status == errSecItemNotFound else {
-			throw KeychainError.deleteFailure(status: status)
+		// Throw an error if an unexpected status was returned
+		switch err {
+		case errSecSuccess:
+			return
+		case errSecItemNotFound:
+			throw KeychainError.itemNotFound
+		default:
+			throw KeychainError.deleteFailure(status: err)
 		}
 	}
 	
-	private static func buildQuery(service: String, account: String) -> [String : AnyObject] {
-		var query = [String : AnyObject]()
-		query[kSecClass as String] = kSecClassGenericPassword
-		query[kSecAttrService as String] = service as AnyObject?
-		query[kSecAttrAccount as String] = account as AnyObject?
-		return query
+	/// Stores a value to a generic password keychain item.
+	///
+	/// This private routine is called to update an existing keychain item.
+	///
+	/// - Parameters:
+	///   - service: The service name for the item.
+	///   - account: The account for the item.
+	///   - password: The desired password.
+	/// - Throws: Any error returned by the Security framework.
+	
+	private static func storeByUpdating(service: String, account: String, password: Data) throws {
+		let err = SecItemUpdate([
+			kSecClass: kSecClassGenericPassword,
+			kSecAttrService: service,
+			kSecAttrAccount: account,
+			] as NSDictionary, [
+				kSecValueData: password
+				] as NSDictionary)
+		guard err == errSecSuccess else {
+			throw KeychainError.writeFailure(status: err)
+		}
+	}
+	
+	/// Stores a value to a generic password keychain item.
+	///
+	/// This private routine is called to add the keychain item.
+	///
+	/// - Parameters:
+	///   - service: The service name for the item.
+	///   - account: The account for the item.
+	///   - password: The desired password.
+	/// - Throws: Any error returned by the Security framework.
+	
+	private static func storeByAdding(service: String, account: String, password: Data) throws {
+		let err = SecItemAdd([
+			kSecClass: kSecClassGenericPassword,
+			kSecAttrService: service,
+			kSecAttrAccount: account,
+			kSecValueData: password,
+			] as NSDictionary, nil)
+		guard err == errSecSuccess else {
+			throw KeychainError.writeFailure(status: err)
+		}
 	}
 }
