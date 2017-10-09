5159fe51284f380dc550d8de02ef92d58d81dd33
Michael Epstein <@mike4aday>
2017-10-09 18:55:05 -0500

    Simplified

--- Pod/Classes/Error.swift
+++ Pod/Classes/Error.swift
@@ -7,17 +7,13 @@
 //
 
 public enum SalesforceError: Error {
+	case deserializationError(message: String?)
 	case resourceException(code: String, message: String, fields: [String]?)
 	case resourceNotFound
 	case serverFailure
 	case unexpectedResponse(response: URLResponse?)
 	case unsupportedURL(url: URL)
 	case userAuthenticationRequired
-}
-
-public enum SerializationError: Error {
-	case missing(String)
-	case invalid(Any, message: String?)
 }
 
 public enum ApplicationError: Error {
@@ -30,24 +26,4 @@ enum KeychainError: Error {
 	case writeFailure(status: OSStatus)
 	case deleteFailure(status: OSStatus)
 	case itemNotFound
-}
-
-extension SalesforceError: CustomDebugStringConvertible {
-	
-	public var debugDescription: String {
-		switch self {
-		case .userAuthenticationRequired:
-			return "User authentication required"
-		case let .unexpectedResponse(response):
-			return "Unexpected response: \(String(describing: response))"
-		case let .unsupportedURL(url):
-			return "Unsupported URL: \(url.absoluteString)"
-		case let .resourceException(code, message, fields):
-			return "Resource exception. Code: \(code). Message: \(message). Fields: \(fields ?? [])"
-		case .resourceNotFound:
-			return "Resource not found"
-		case .serverFailure:
-			return "Server failure"
-		}
-	}
 }
