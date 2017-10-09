5e63a6a48c8f01c7f01dbc7962bd06211a93240f
Michael Epstein <@mike4aday>
2017-10-08 22:16:29 -0500

    Implements Decodable protocol

--- Pod/Classes/QueryResult.swift
+++ Pod/Classes/QueryResult.swift
@@ -9,23 +9,9 @@
 
 /// Holds the result of a SOQL query
 /// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/dome_query.htm
-public struct QueryResult {
-	
+public struct QueryResult<T: Decodable>: Decodable {
 	public let totalSize: Int
 	public let isDone: Bool
-	public let records: [Record]
+	public let records: [T]
 	public let nextRecordsPath: String?
-	
-	public init(json: [String: Any]) throws {
-		guard
-			let totalSize = json["totalSize"] as? Int,
-			let isDone = json["done"] as? Bool,
-			let records = json["records"] as? [[String: Any]] else {
-				throw SerializationError.invalid(json, message: "Unable to create QueryResult")
-		}
-		self.totalSize = totalSize
-		self.isDone = isDone
-		self.records = records
-		self.nextRecordsPath = json["nextRecordsUrl"] as? String
-	}
 }
