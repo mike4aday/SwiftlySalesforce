e81e3526ae957c57dda8abcd63bc1b305f210427
Michael Epstein <@mike4aday>
2017-10-08 22:02:14 -0500

    Implements Decodable protocol

--- Pod/Classes/Limit.swift
+++ Pod/Classes/Limit.swift
@@ -8,18 +8,13 @@
 
 /// Represents a limited Salesforce resource
 /// See: https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_limits.htm
-public struct Limit {
+public struct Limit: Decodable {
 	
-	public let name: String
 	public let maximum: Int
 	public let remaining: Int
 	
-	public init(name: String, json: [String: Any]) throws {
-		guard let remaining = json["Remaining"] as? Int, let maximum = json["Max"] as? Int else {
-			throw SerializationError.invalid(json, message: "Unable to create Limit")
-		}
-		self.name = name
-		self.maximum = maximum
-		self.remaining = remaining
+	enum CodingKeys: String, CodingKey {
+		case maximum = "Max"
+		case remaining = "Remaining"
 	}
 }
