0353f28475a444337a93dfe2f6136a98e248e489
Michael Epstein <@mike4aday>
2017-10-09 16:24:55 -0500

    Updated coding keys

--- Pod/Classes/QueryResult.swift
+++ Pod/Classes/QueryResult.swift
@@ -10,8 +10,16 @@
 /// Holds the result of a SOQL query
 /// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/dome_query.htm
 public struct QueryResult<T: Decodable>: Decodable {
+	
 	public let totalSize: Int
 	public let isDone: Bool
 	public let records: [T]
 	public let nextRecordsPath: String?
+	
+	enum CodingKeys: String, CodingKey {
+		case totalSize
+		case isDone = "done"
+		case records
+		case nextRecordsPath = "nextRecordsUrl"
+	}
 }
