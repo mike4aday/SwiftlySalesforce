2d6b564323b38dcbf3da15936a3ed4b5414a6fd6
Michael Epstein <@mike4aday>
2017-10-10 22:50:24 -0500

    Cleaned up test logic

--- Example/Tests/IdentityTests.swift
+++ Example/Tests/IdentityTests.swift
@@ -11,11 +11,11 @@ import XCTest
 
 class IdentityTests: XCTestCase, MockData {
 	
-	var json: [String: Any]!
+	var decoder = JSONDecoder(dateFormatter: DateFormatter.salesforceDateTimeFormatter)
 	
     override func setUp() {
 		super.setUp()
-		json = readJSONDictionary(fileName: "MockIdentity")!
+		decoder.dateDecodingStrategy = JSONDecoder.DateDecodingStrategy.formatted(DateFormatter.salesforceDateTimeFormatter)
     }
     
     override func tearDown() {
@@ -24,41 +24,25 @@ class IdentityTests: XCTestCase, MockData {
 	
 	func testThatItInits() {
 		
-		// Given
-		
-		// When
-		guard let identity = try? Identity(json: json) else {
+		guard let data = read(fileName: "MockIdentity") else {
 			return XCTFail()
 		}
 		
-		// Then
-		XCTAssertNotNil(identity.displayName)
-		XCTAssertNotNil(identity.lastModifiedDate)
-		XCTAssertNotNil(identity.orgID)
-		XCTAssertNotNil(identity.userID)
-		XCTAssertNotNil(identity.userType)
-		
-		XCTAssertEqual(identity.displayName, "Martin Van Nostrand")
-		XCTAssertNil(identity.mobilePhone)
-		XCTAssertEqual(identity.username, "martin@vandelayindustries.com")
-		XCTAssertEqual(identity.userID, "005i00000016PdaBAE")
-		XCTAssertEqual(identity.orgID, "00Di0000000bcK3FAI")
-		XCTAssertEqual(identity.userType, "STANDARD")
-		XCTAssertEqual(identity.language!, "en_US")
-		XCTAssertEqual(identity.lastModifiedDate, DateFormatter.salesforceDateTimeFormatter.date(from: "2017-03-13T16:11:13.000+0000"))
-		XCTAssertEqual(identity.locale!, "en_US")
-		XCTAssertEqual(identity.thumbnailURL!, URL(string: "https://c.na85.content.force.com/profilephoto/005/T"))
-	}
-	
-	func testThatItWontInt() {
-		
-		// Given
-		json["user_id"] = nil
-		
-		// When
-		let identity = try? Identity(json: json)
-		
-		// Then
-		XCTAssertNil(identity)
+		do {
+			let identity = try decoder.decode(Identity.self, from: data)
+			XCTAssertEqual(identity.displayName, "Martin Van Nostrand")
+			XCTAssertNil(identity.mobilePhone)
+			XCTAssertEqual(identity.username, "martin@vandelayindustries.com")
+			XCTAssertEqual(identity.userID, "005i00000016PdaBAE")
+			XCTAssertEqual(identity.orgID, "00Di0000000bcK3FAI")
+			XCTAssertEqual(identity.userType, "STANDARD")
+			XCTAssertEqual(identity.language!, "en_US")
+			XCTAssertEqual(identity.lastModifiedDate, DateFormatter.salesforceDateTimeFormatter.date(from: "2017-03-13T16:11:13.000+0000"))
+			XCTAssertEqual(identity.locale!, "en_US")
+			XCTAssertEqual(identity.thumbnailURL!, URL(string: "https://c.na85.content.force.com/profilephoto/005/T"))
+		}
+		catch {
+			return XCTFail(String(describing: error))
+		}
 	}
 }
