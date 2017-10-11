dadd40138d9bbd55028726254d7e23fa9e989b47
Michael Epstein <@mike4aday>
2017-10-10 22:52:57 -0500

    Simplified test logic

--- Example/Tests/IdentityTests.swift
+++ Example/Tests/IdentityTests.swift
@@ -24,21 +24,16 @@ class IdentityTests: XCTestCase, MockData {
 	
 	func testThatItInits() {
 		let data = read(fileName: "MockIdentity")!
-		do {
-			let identity = try decoder.decode(Identity.self, from: data)
-			XCTAssertEqual(identity.displayName, "Martin Van Nostrand")
-			XCTAssertNil(identity.mobilePhone)
-			XCTAssertEqual(identity.username, "martin@vandelayindustries.com")
-			XCTAssertEqual(identity.userID, "005i00000016PdaBAE")
-			XCTAssertEqual(identity.orgID, "00Di0000000bcK3FAI")
-			XCTAssertEqual(identity.userType, "STANDARD")
-			XCTAssertEqual(identity.language!, "en_US")
-			XCTAssertEqual(identity.lastModifiedDate, DateFormatter.salesforceDateTimeFormatter.date(from: "2017-03-13T16:11:13.000+0000"))
-			XCTAssertEqual(identity.locale!, "en_US")
-			XCTAssertEqual(identity.thumbnailURL!, URL(string: "https://c.na85.content.force.com/profilephoto/005/T"))
-		}
-		catch {
-			return XCTFail(String(describing: error))
-		}
+		let identity = try! decoder.decode(Identity.self, from: data)
+		XCTAssertEqual(identity.displayName, "Martin Van Nostrand")
+		XCTAssertNil(identity.mobilePhone)
+		XCTAssertEqual(identity.username, "martin@vandelayindustries.com")
+		XCTAssertEqual(identity.userID, "005i00000016PdaBAE")
+		XCTAssertEqual(identity.orgID, "00Di0000000bcK3FAI")
+		XCTAssertEqual(identity.userType, "STANDARD")
+		XCTAssertEqual(identity.language!, "en_US")
+		XCTAssertEqual(identity.lastModifiedDate, DateFormatter.salesforceDateTimeFormatter.date(from: "2017-03-13T16:11:13.000+0000"))
+		XCTAssertEqual(identity.locale!, "en_US")
+		XCTAssertEqual(identity.thumbnailURL!, URL(string: "https://c.na85.content.force.com/profilephoto/005/T"))
 	}
 }
