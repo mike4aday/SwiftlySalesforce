560d8f0cd130eb29b12b8602557c0e3f9dceba00
Michael Epstein <@mike4aday>
2017-10-11 08:11:37 -0500

    Simplified test logic

--- Example/Tests/ResourceTests.swift
+++ Example/Tests/ResourceTests.swift
@@ -16,45 +16,30 @@ class ResourceTests: XCTestCase {
 	let identityURL = URL(string: "https://login.salesforce.com/id/00Di0000000XXX3EAI/005i00000016PdaAAE")!
 	let instanceURL = URL(string: "https://na15.salesforce.com")!
 	
-	func testIdentityRequest() {
+	func testIdentity() {
 		
-		// Given
 		let authData = OAuth2Result(accessToken: accessToken, instanceURL: instanceURL, identityURL: identityURL, refreshToken: refreshToken)
 		
-		// When
-		guard let req = try? Resource.identity(version: "41.0").asURLRequest(authData: authData) else {
-			return XCTFail()
-		}
+		let req = try! Resource.identity(version: "41.0").asURLRequest(authData: authData)
 		
-		// Then
 		XCTAssertEqual(req.httpMethod, HTTPMethod.get.rawValue)
 		XCTAssertEqual(req.url!.absoluteString, identityURL.absoluteString + "?version=41.0")
 		XCTAssertEqual(req.url!.value(forQueryItem: "version"), "41.0")
+		XCTAssertEqual(req.value(forHTTPHeaderField: "Authorization")!, "Bearer ACCESS_TOKEN")
 	}
 	
-	func testRetrieveRequest() {
+	func testRetrieve() {
 		
-		// Given
 		let authData = OAuth2Result(accessToken: accessToken, instanceURL: instanceURL, identityURL: identityURL, refreshToken: refreshToken)
 		let type = "Account"
 		let id = "12345"
 		let fields = ["Id","Name","Custom1__c"]
 		let version = "41.0"
 		
-		// When
-		guard let req = try? Resource.retrieve(type: type, id: id, fields: fields, version: version).asURLRequest(authData: authData),
-			let url = req.url,
-			let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
-			return XCTFail()
-		}
+		let req = try! Resource.retrieve(type: type, id: id, fields: fields, version: version).asURLRequest(authData: authData)
 		
-		// Then
+		XCTAssertEqual(req.url!.absoluteString, "https://na15.salesforce.com/services/data/v41.0/sobjects/Account/12345?fields=Id,Name,Custom1__c")
 		XCTAssertEqual(req.httpMethod, HTTPMethod.get.rawValue)
-		XCTAssertEqual(url.path, "/services/data/v\(version)/sobjects/\(type)/\(id)")
-		XCTAssert(components.queryItems!.count == 1)
-		XCTAssert(components.queryItems![0].name == "fields")
-		XCTAssert(components.queryItems![0].value!.contains("Id"))
-		XCTAssert(components.queryItems![0].value!.contains("Name"))
-		XCTAssert(components.queryItems![0].value!.contains("Custom1__c"))
+		XCTAssertEqual(req.value(forHTTPHeaderField: "Authorization")!, "Bearer ACCESS_TOKEN")
 	}
 }
