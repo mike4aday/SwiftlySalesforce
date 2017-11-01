6beabb82365366b0b7d8f92f355d81c33130e83c
Michael Epstein <@mike4aday>
2017-11-01 13:20:24 -0500

    Added Encodable compliance

--- SwiftlySalesforce/Classes/Address.swift
+++ SwiftlySalesforce/Classes/Address.swift
@@ -9,9 +9,9 @@
 /// Standard objects' compound address field data. 
 /// See https://developer.salesforce.com/docs/atlas.en-us.api.meta/api/compound_fields_address.htm
 
-public struct Address: Decodable {
+public struct Address: Codable {
 	
-	public enum GeocodeAccuracy: String, Decodable {
+	public enum GeocodeAccuracy: String, Codable {
 		case address = "Address"
 		case nearAddress = "NearAddress"
 		case block = "Block"
