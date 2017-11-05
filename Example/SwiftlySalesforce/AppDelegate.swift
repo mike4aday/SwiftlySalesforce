64a6cb2451175fc20342df7aa3c857c09e323524
Michael Epstein <@mike4aday>
2017-11-04 19:15:01 -0500

    Simplified `Salesforce` configuration

--- Example/SwiftlySalesforce/AppDelegate.swift
+++ Example/SwiftlySalesforce/AppDelegate.swift
@@ -16,75 +16,17 @@ var salesforce: Salesforce!
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, LoginDelegate {
 
 	let consumerKey = "<YOUR CONNECTED APP'S CONSUMER KEY HERE>"
-	let redirectURL = URL(string: "<YOUR CONNECTED APP'S REDIRECT URL HERE>")!
+	let callbackURL = URL(string: "<YOUR CONNECTED APP'S REDIRECT URL HERE>")!
 	
-	var connectedApp: ConnectedApp!
 	var window: UIWindow?
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		
-		connectedApp = ConnectedApp(consumerKey: consumerKey, redirectURL: redirectURL, loginDelegate: self)
-		salesforce = Salesforce(connectedApp: connectedApp)
-		
-        //Uncomment if you want to receive push notifications, including those
-        //from Salesforce's universal notification service
-        //registerForRemoteNotification()
-		
+		salesforce = configureSalesforce(consumerKey: consumerKey, callbackURL: callbackURL)
 		return true
 	}
 	
 	func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-		handleRedirectURL(url, for: connectedApp)
+		handleCallbackURL(url, for: salesforce.connectedApp)
 		return true
 	}
-    
-    //
-    //MARK: Push notifications
-    // This code below is only required if you care about push notifications.
-    //
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
-        salesforce.registerForNotifications(deviceToken: deviceTokenString)
-            .then {
-                (result) -> () in
-                print("Successfully registered for salesforce notifications")
-            }.catch {
-                error in
-                print(error)
-        }
-    }
-    
-    //Called when a notification is delivered to a foreground app.
-    @available(iOS 10.0, *)
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        print("User Info = ",notification.request.content.userInfo)
-        completionHandler([.alert, .badge, .sound])
-    }
-    
-    //Called to let your app know which action was selected by the user for a given notification.
-    @available(iOS 10.0, *)
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        print("didReceive = ",response.notification.request.content.userInfo)
-        
-        completionHandler()
-        
-    }
-    
-    func registerForRemoteNotification() {
-        if #available(iOS 10.0, *) {
-            let center  = UNUserNotificationCenter.current()
-            center.delegate = self
-            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-                if error == nil{
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            }
-        }
-        else {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-        UIApplication.shared.applicationIconBadgeNumber = 0
-    }
-
 }
