9c2c1034847ecfdae3465525e419c166df156439
Michael Epstein <mike4aday@users.noreply.github.com>
2018-08-10 13:45:03 -0500

    Added documentation for batchSize argument

--- SwiftlySalesforce/Sources/Extensions/Salesforce+Query.swift
+++ SwiftlySalesforce/Sources/Extensions/Salesforce+Query.swift
@@ -14,7 +14,7 @@ public extension Salesforce {
 	/// Asynchronsouly executes a SOQL query.
 	/// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_query.htm
 	/// - Parameter soql: SOQL query
-	/// - Parameter batchSize: maximum number of records returned per result set (i.e. pagination)
+	/// - Parameter batchSize: number of records returned per result set; minimum 200, maximum 2000
 	/// - Parameter options: if you want to defer login, set to [.dontAuthenticate]
 	/// - Returns: Promise of a QueryResult whose records, if any, are decoded as generic type 'T'
 	public func query<T: Decodable>(soql: String, batchSize: Int? = nil, options: Options = []) -> Promise<QueryResult<T>> {
@@ -25,7 +25,7 @@ public extension Salesforce {
 	/// Asynchronsouly executes a SOQL query
 	/// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_query.htm
 	/// - Parameter soql: SOQL query
-	/// - Parameter batchSize: number of records returned per result set; minimum 200, maximum 2000.
+	/// - Parameter batchSize: number of records returned per result set; minimum 200, maximum 2000
 	/// - Parameter options: if you want to defer login, set to [.dontAuthenticate]
 	/// - Returns: Promise of a QueryResult whose records, if any, are decoded as SObjects
 	public func query(soql: String, batchSize: Int? = nil, options: Options = []) -> Promise<QueryResult<SObject>> {
@@ -36,7 +36,7 @@ public extension Salesforce {
 	/// Asynchronsouly executes multiple SOQL queries in parallel.
 	/// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/resources_query.htm
 	/// - Parameter soql: Array of SOQL queries
-	/// - Parameter batchSize: number of records returned per result set; minimum 200, maximum 2000.
+	/// - Parameter batchSize: number of records returned per result set; minimum 200, maximum 2000
 	/// - Parameter options: if you want to defer login, set to [.dontAuthenticate]
 	/// - Returns: Promise of an array of QueryResults, in the same order as the "soql" parameter
 	public func query<T: Decodable>(soql: [String], batchSize: Int? = nil, options: Options = []) -> Promise<[QueryResult<T>]> {
@@ -51,7 +51,7 @@ public extension Salesforce {
 	
 	/// Queries next pages of records returned by a SOQL query whose result is broken into multiple pages.
 	/// See https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/dome_query.htm
-	/// - Parameter path: the 'nextRecordsPath' property of a previously-obtained QueryResult.
+	/// - Parameter path: the 'nextRecordsPath' property of a previously-obtained QueryResult
 	/// - Parameter options: if you want to defer login, set to [.dontAuthenticate]
 	/// - Returns: Promise of a QueryResult
 	public func queryNext<T: Decodable>(path: String, options: Options = []) -> Promise<QueryResult<T>> {
