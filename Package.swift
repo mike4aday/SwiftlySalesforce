c21ccdcaa8a17df78f00f77d44d93fee5795d60d
Michael Epstein <mike@efactory.com>
2022-03-27 12:49:23 -0500

    Updated platform version to iOS 15

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftlySalesforce",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
