512f00db5cccf5b44956b786331a4b190974c2fe
Michael Epstein <mike@efactory.com>
2022-05-27 23:20:18 -0500

    Made JSONEncoder helper extension public

--- Sources/SwiftlySalesforce/Extensions/JSONEncoder+Helpers.swift
+++ Sources/SwiftlySalesforce/Extensions/JSONEncoder+Helpers.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // Borrowed from https://stackoverflow.com/questions/63077827/how-can-i-encode-a-codable-type-by-specifying-a-subset-of-its-codingkeys/63080358
-extension JSONEncoder {
+public extension JSONEncoder {
     
     func withEncodeSubset<CodingKeys>(keysToEncode: [CodingKeys]) -> JSONEncoder {
         userInfo[.keysToEncode] = keysToEncode
